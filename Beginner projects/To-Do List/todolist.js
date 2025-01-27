6532e23b7fc54625a0f29dcdb062763e90fa7351
Esa <esa.aaro.pistemaa@gmail.com>
2025-01-27 21:08:30 +0200

    Organizing files

--- Beginner projects/To-Do List/todolist.js
+++ Beginner projects/To-Do List/todolist.js
@@ -5,16 +5,17 @@ let testvariable = 'bitchboy';
 function addTask() {
     let userInput = document.getElementById('taskInput').value;
     if (userInput) {
-    let item = `<div id="task-container">
+    let item = `
+            <div id="task-container">
             <p id="userInput">${userInput}</p>
-            <button onclick="deleteTask" id="deleteBtn">Poista</button>
+            <button onclick="deleteTask()" id="deleteBtn">Poista</button>
             </div>`;
     document.getElementById('tasks').innerHTML += item;
-    document.getElementById('taskInput').innerHTML = '';
+    //document.getElementById('taskInput').value = '';
     }
 
 }
 
 function deleteTask() {
-
+    document.getElementById('task-container').innerHTML = null;
 }
