bdd298914620719d9afc03f603f89447995b40d9
Esa <esa.aaro.pistemaa@gmail.com>
2025-01-30 23:15:10 +0200

    todolist interactable background

--- Beginner projects/To-Do List/todolist.js
+++ Beginner projects/To-Do List/todolist.js
@@ -1,14 +1,20 @@
 let userInput;
 let testvariable = 'bitchboy';
+let increment = 1;
 
 
 function addTask() {
     let userInput = document.getElementById('taskInput').value;
     if (userInput) {
-    let item = `
-            <div id="task-container">
+    idIncrement();
+    console.log(increment);
+    let item = `<div id="${increment}">
+            <div id="row">
+            <button onclick="button()" id="task-container">
             <p id="userInput">${userInput}</p>
-            <button onclick="deleteTask()" id="deleteBtn">Poista</button>
+            <button onclick="deleteTask(${increment})" id="deleteBtn">Poista</button>
+            </button>
+            </div>
             </div>`;
     document.getElementById('tasks').innerHTML += item;
     //document.getElementById('taskInput').value = '';
@@ -16,6 +22,16 @@ function addTask() {
 
 }
 
-function deleteTask() {
-    document.getElementById('task-container').innerHTML = null;
+function idIncrement() {
+    increment++;
+    return increment;
+}
+
+function deleteTask(idNumber) {
+    document.getElementById(idNumber).remove();
+
+}
+
+function button() {
+    console.log('button');
 }
