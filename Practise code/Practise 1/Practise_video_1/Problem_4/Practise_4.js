e5ce8fce6e2ad3809915c1c7c66ee67f14cf2424
Esa <esa.aaro.pistemaa@gmail.com>
2025-04-10 08:23:32 +0300

    Added practise_4.js

---
+++ Practise code/Practise 1/Practise_video_1/Problem_4/Practise_4.js
@@ -0,0 +1,8 @@
+const day = new Date().getDate();
+const month = new Date().getMonth() + 1;
+const year = new Date().getFullYear();
+const date = day + "/" + month + "/" + year;
+console.log(day);
+console.log(month);
+console.log(year);
+console.log(date);
