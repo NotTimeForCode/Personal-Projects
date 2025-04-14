b605080834927a694b5ab07156585ca28a0aae19
Esa <esa.aaro.pistemaa@gmail.com>
2025-04-14 22:18:46 +0300

    Added and begun Dog Matcher project Practised variable and typeof operator

---
+++ Practise code/test folder/random.js
@@ -0,0 +1,12 @@
+let a = "hello world";
+let b = 10;
+let c = true;
+let d = null;
+let e = undefined;
+let f = Symbol("symbol");
+let g = BigInt(1234567890123456789012345678901234567890n);
+let h = { name: "John", age: 30 };
+
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g, typeof h);
+
+console.log(Number.MAX_SAFE_INTEGER);
